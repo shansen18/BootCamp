15ff88d5aa9af87f32bdbd9b591ee786b021ab0b
shansen18 <84162628+shansen18@users.noreply.github.com>
2021-07-13 13:22:08 -0500

    Update roulette_dealer_finder_by_time.sh
    added the clear, and echo to make the return look nicer, also added the comments in the code

--- Week3/roulette_dealer_finder_by_time.sh
+++ Week3/roulette_dealer_finder_by_time.sh
@@ -1,8 +1,16 @@
  #!/bin/bash/
 
+#Prompts for user to put in the variable
 read -p 'Input Date (four numbers): ' da
 read -p 'Input time: ' ti
 read -p 'Input A for AM or P for PM: ' am
 
-awk '/'${ti}':00:00 '${am}'M/ {print $1,$2,$5,$6}' ${da}_Dealer_schedule
+#clears the screen
+clear
+
+#this echos whats in "" with the variables. 
+echo "the dealer on $da at '$ti':00 '$am'M was"
+
+#this is the code for the search based on the variables given
+awk '/'${ti}':00:00 '${am}'M/ {print $5,$6}' ${da}_Dealer_schedule
 
